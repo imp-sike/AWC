fefff1735fe6600008b908363567a6d57de26b89
imp-sike <sulavparajuli07@gmail.com>
2022-09-29 10:35:00 +0545

    Added OpenGl Rendering Settings

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-echo "Copying assets..."
-cp -r ./assets ./android/Sources/assets
+echo "Deleting previous build..."
 rm -R build
 rm demo.apk
 rm demo.apk.idsig
+
+
+echo "Copying assets..."
+cp -r ./assets/* ./android/Sources/assets
 
 echo "Build Running..."
 make push run
--- lib/launch.c
+++ lib/launch.c
@@ -1,13 +1,54 @@
 #include "framework.c"
 
-void ui() {
-	TitleBar("Test", 0x000000ff, 0xffffffff);
-	Text("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-	Text("abcdefghijklmnopqrstuvwxyz\n0123456789");
+short screenx, screeny;
+
+bool pressed = false;
+char *text = "Button Not Clicked";
+image *backgroud1;
+image *ground;
+
+void onClick()
+{
+    if (pressed)
+        text = "Button Clicked";
+    else
+        text = "Button Not Clicked";
+    pressed = !pressed;
+}
+
+void initialize_assets()
+{
+    backgroud1 = loadimagefromapk("background.png");
+    backgroud1->tex = CNFGTexImage(backgroud1->rdimg, backgroud1->w, backgroud1->h);
+
+
+    ground = loadimagefromapk("ground.png");
+    ground->tex = CNFGTexImage(ground->rdimg, ground->w, ground->h);
+}
+
+void init()
+{
+    initialize_assets();
+}
+
+void HandleResume()
+{
+    suspended = 0;
+    initialize_assets();
+}
+
+void gameloop()
+{
+    CNFGGetDimensions(&screenx, &screeny);
+    CNFGBGColor = 0x000000ff;
+    CNFGFlushRender();
+    RenderImage(backgroud1->tex, 0, 0, screenx, screeny);
+    // Button(onClick, text, 0xffffffff, 0x000000ff);
+    RenderImage(ground->tex, 0, screenx - 0.6 * screenx, screenx, screeny);
 }
 
 int main()
 {
-	run(ui);
-	return 0;
+    run(init, gameloop);
+    return 0;
 }
